b951bd6caf4132fb606555147d093ccecc022c6a
Ryan <ryan.b.a@live.nl>
2025-12-02 16:08:41 +0100

    Urenbon.cs veranderd naar HoursReceipt.cs

---
+++ UrenRegistratie.Core/Models/HoursReceipt.cs
@@ -0,0 +1,12 @@
+﻿namespace UrenRegistratie.Core.Models;
+
+public class HoursReceipt
+{
+    public DateTime Date {get; set;}
+    
+
+    public HoursReceipt(DateTime date)
+    {
+        Date = date;
+    }
+}
