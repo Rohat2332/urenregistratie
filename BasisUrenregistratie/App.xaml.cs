d410452126e4c8721bdf84c08d06bd90f7feb295
Ryan <ryan.b.a@live.nl>
2025-12-02 16:22:04 +0100

    EmployeeOverview.xaml - made the view so that it shows the HoursReceipts for the user EmployeeOverview.xaml.cs - Added the viewmodel EmployeeOverviewViewModel
    MauiProgram.cs
    - added communitytoolkit
    - added services voor employeeOverview view and viewmodel
    
    UrenApp.sln
    - Change for HourRegistration
    
    App.xaml.cs
    - added view voor EmployeeOverviewViemodel
    
    added gitignore

--- BasisUrenregistratie/App.xaml.cs
+++ BasisUrenregistratie/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace BasisUrenregistratie;
+﻿using BasisUrenregistratie.ViewModels;
+using BasisUrenregistratie.Views;
+
+namespace BasisUrenregistratie;
 
 public partial class App : Application
 {
-    public App()
+    public App(EmployeeOverviewViewModel viewModel)
     {
         InitializeComponent();
 
-        MainPage = new AppShell();
+        MainPage = new EmployeeOverview(viewModel);
     }
 }
--- BasisUrenregistratie/Views/EmployeeOverview.xaml.cs
+++ BasisUrenregistratie/Views/EmployeeOverview.xaml.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasisUrenregistratie.ViewModels;
 
 namespace BasisUrenregistratie.Views;
 
-public partial class WerknemerOverzicht : ContentPage
+public partial class EmployeeOverview : ContentPage
 {
-    public WerknemerOverzicht()
+    public EmployeeOverview(EmployeeOverviewViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
