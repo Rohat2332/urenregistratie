fa5c9df0f11429067888131c27d7264f97635ce5
Ryan <ryan.b.a@live.nl>
2025-12-04 16:21:08 +0100

    HourReceipt.cs Aangepast. Heeft nu Id, UserId, ProjectId, Status, HoursWorked, MinutesWorked, Remark.
    Constructor bevat nu (int id, int userId, DateTime date, string status)

---
+++ HourRegistration.Core/Models/HourReceipt.cs
@@ -0,0 +1,22 @@
+﻿namespace UrenRegistratie.Core.Models;
+
+public class HourReceipt
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ProjectId { get; set; }
+    public string Status { get; set; }
+    public int HoursWorked { get; set; }
+    public int MinutesWorked { get; set; }
+    public string Remark { get; set; }
+    public DateTime Date {get; set;}
+    
+
+    public HourReceipt(int id,int userId, DateTime date, string status)
+    {
+        Id = id;
+        UserId = userId;
+        Date = date;
+        Status = status;
+    }
+}
