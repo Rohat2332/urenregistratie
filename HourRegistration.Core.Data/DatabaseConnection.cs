702821a07576a180f2815279fade83673172b8d6
Ryan <ryan.b.a@live.nl>
2025-12-04 16:30:36 +0100

    Rest van de commits

---
+++ HourRegistration.Core.Data/DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using UrenRegistratie.Core.Interfaces.Services;
+
+namespace HourRegistartion.Core.Data;
+
+public class DatabaseConnection
+{
+    private readonly IConfigurationService _configurationService;
+    private readonly string _connectionStringName = "HourRegistartion";
+
+    public DatabaseConnection(IConfigurationService configurationService)
+    {
+        _configurationService = configurationService;
+    }
+
+    public async Task<MySqlConnection> GetOpenConnectionAsync()
+    {
+        string connectionString = _configurationService.GetSetting(_connectionStringName);
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException($"Connection string '{_connectionStringName}' not found in configuration.");
+        }
+
+        try
+        {
+            var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            
+            return connection;
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine($"Error connecting to MySQL: {ex.Message}");
+            throw; 
+        }
+    }
+}
---
+++ HourRegistration.Core/Services/ConfigurationService.cs
@@ -0,0 +1,39 @@
+﻿using UrenRegistratie.Core.Interfaces.Services;
+
+namespace UrenRegistratie.Core.Services;
+
+public class ConfigurationService : IConfigurationService
+{
+    private readonly IConfigurationService _configuration;
+
+    public ConfigurationService(IConfigurationService configuration)
+    {
+        _configuration = configuration;
+    }
+
+
+    public string GetSetting(string key, string defaultValue = "")
+    {
+        return _configuration[key] ?? defaultValue;
+    }
+
+    public bool GetBoolSetting(string key, bool defaultValue = false)
+    {
+        string value = GetSetting(key);
+
+        if (string.IsNullOrEmpty(value))
+        {
+            return defaultValue;
+        }
+
+        if (bool.TryParse(value, out bool result))
+        {
+            return result;
+        }
+
+        // Return default if parsing fails
+        return defaultValue;
+    }
+
+    public string this[string key] => throw new NotImplementedException();
+}
