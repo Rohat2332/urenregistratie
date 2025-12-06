607431efbe8802b3bf6fc024bd37825b784a690b
Ryan <ryan.b.a@live.nl>
2025-12-06 23:18:14 +0100

    WeeklyDaySummary.cs fix: - namespace naar juiste namespace gezet

---
+++ HourRegistration.Core/Models/WeeklyDaySummary.cs
@@ -0,0 +1,10 @@
+﻿namespace UrenRegistratie.Core.Models;
+
+/// <summary>
+/// Represents a summary of a specific day within a weekly schedule.
+/// </summary>
+public class WeeklyDaySummary
+{
+    public DateTime Date { get; set; }
+    public double TotalHours { get; set; }
+}
