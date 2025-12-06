5637b60c2e6f084f4be24bb4d4a04d6852bcb2b5
Ryan <ryan.b.a@live.nl>
2025-12-06 18:46:34 +0100

    HourReceipt.cs - Aanpassing gemaakt aan de Status en Remark variabelen - Aanpassing gemaakt aan de constructor

--- HourRegistration.Core/Models/HourReceipt.cs
+++ HourRegistration.Core/Models/HourReceipt.cs
@@ -5,18 +5,23 @@ public class HourReceipt
     public int Id { get; set; }
     public int UserId { get; set; }
     public int ProjectId { get; set; }
-    public string Status { get; set; }
+    public string? Status { get; set; }
     public int HoursWorked { get; set; }
     public int MinutesWorked { get; set; }
-    public string Remark { get; set; }
+    public string? Remark { get; set; }
     public DateTime Date {get; set;}
-    
 
-    public HourReceipt(int id,int userId, DateTime date, string status)
+    public HourReceipt(int id, int userId, int projectId, string status, int hoursWorked, int minutesWorked,string remark, DateTime date)
     {
         Id = id;
         UserId = userId;
-        Date = date;
+        ProjectId = projectId;
         Status = status;
+        HoursWorked = hoursWorked;
+        MinutesWorked = minutesWorked;
+        Remark = remark;
+        Date = date;
     }
+
+    
 }
